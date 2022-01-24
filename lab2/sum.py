729ebbab1cd3397b94aeca6c001b3fa813b5aab7
eric_martin <ericmartin657@gmail.com>
2022-01-24 11:19:01 -0800

    added #!/usr/bin/env python to lab 2 files and edited sum.py

---
+++ lab2/sum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+if __name__ =="__main__":
+    sum(list)
+
+def sum():
+    initial=0
+    for i in list:
+        initial+=i
+    return initial,print(sum(list))
+
+
+sum([1,2,3])
