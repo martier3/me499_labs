4a3591d318ab5272397041e721f841433f45d8bf
eric_martin <ericmartin657@gmail.com>
2022-01-26 18:06:36 -0800

    sum_r now kinda works, the unput only functions if the input is surrounded by brackets

--- lab2/sum.py
+++ lab2/sum.py
@@ -10,11 +10,21 @@ sum_i(1,2,3)
 
 s = sum_i(1)
 
-def sum_r(*args):
-    n = 0
-    if n == 1:
-        return 0
-    else:
-        return (n+sum_r(*args-1)), print(sum_r)
+def listsum(numList):
+   if len(numList) == 1:
+        return numList[0]
+   else:
+        return numList[0] + listsum(numList[1:])
 
-sum_r(1,2,3)
+print(listsum([1,3,5,7,9]))
+
+def sum_r(numberlist):
+   if len(numberlist) == 1:
+        return numberlist[0]
+   else:
+       total = numberlist[0] + sum_r(numberlist[1:])
+   return total
+
+print(sum_r([1,3,5,7,9]))
+
+s = sum_r([1])
