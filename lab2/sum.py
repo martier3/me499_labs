a361270fa1633f0e587b9c37f50e4ef844cc66d2
eric_martin <ericmartin657@gmail.com>
2022-01-26 17:45:16 -0800

    sum_i works, sum_r doesn't

--- lab2/sum.py
+++ lab2/sum.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
-if __name__ =="__main__":
-    sum(list)
+def sum_i(*args):
+    total = 0
+    for i in args:
+        total += i
+    return total, print(total)
 
-def sum():
-    initial=0
-    for i in list:
-        initial+=i
-    return initial,print(sum(list))
+sum_i(1,2,3)
 
+s = sum_i(1)
 
-sum([1,2,3])
+def sum_r(*args):
+    n = 0
+    if n == 1:
+        return 0
+    else:
+        return (n+sum_r(*args-1)), print(sum_r)
+
+sum_r(1,2,3)
