895d366ea590e88dcbdca428955c7d2ff3dcb55a
eric_martin <ericmartin657@gmail.com>
2022-01-27 19:37:36 -0800

    Sadie made all the files work

--- lab2/sum.py
+++ lab2/sum.py
@@ -10,21 +10,25 @@ sum_i(1,2,3)
 
 s = sum_i(1)
 
+
+"""
 def listsum(numList):
    if len(numList) == 1:
         return numList[0]
    else:
         return numList[0] + listsum(numList[1:])
-
 print(listsum([1,3,5,7,9]))
+"""
 
-def sum_r(numberlist):
-   if len(numberlist) == 1:
-        return numberlist[0]
+
+def sum_r(*args):
+   if len(args) == 1:
+        return args[0]
    else:
-       total = numberlist[0] + sum_r(numberlist[1:])
+       total = args[0] + sum_r(args[:1])
    return total
 
-print(sum_r([1,3,5,7,9]))
+print(sum_r([1,2,3,4,5,6]))
 
-s = sum_r([1])
+s = sum_r[1]
+print(s)
