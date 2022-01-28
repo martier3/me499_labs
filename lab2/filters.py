ebd4cab6d430d6a47442559a982c356ba4ae05ca
eric_martin <ericmartin657@gmail.com>
2022-01-27 19:37:18 -0800

    Sadie made all the files work

--- lab2/filters.py
+++ lab2/filters.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
 
+def mean_filter(data, cutoff=0.8):
+    """
+    Apply a mean filter to an iterable, returning a list.
+    """
+
+    # Make an empty list
+    filtered = []
+
+    # Add one element at a time to the list.  There are a lot of ways to do this; this is just one of them.
+    for datum in data:
+        datum = min(datum, cutoff)
+        filtered.append(datum)
+
+    # Return the new list
+    return filtered
--- lab2/test_filter.py
+++ lab2/test_filter.py
@@ -1,1 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+from sensor import generate_sensor_data
+from amp_filter import apply_amp_filter
+import csv
+
+n = 1000
+data = generate_sensor_data(n,)
+filter_data = apply_amp_filter(data, 0.8)
+compile_data = (data, filter_data)
+
+filename = "test_filter.csv"
+
+with open(filename, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for i in range(n):
+        csvwriter.writerow(["{:.4f}".format(data[i]), "{:.4f}".format(filter_data[i])])
