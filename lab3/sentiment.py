324df6488841317a005191882e4f4a7194447c2d
martier3 <ericmartin657@gmail.com>
2022-02-03 21:03:05 -0800

    everything works. need to figure out if name main thing

--- lab3/sentiment.py
+++ lab3/sentiment.py
@@ -6,10 +6,10 @@ import re
 txt_dictionary = {}
 output_string = []
 
-def load_score_dict(text_file = 'sentiment.txt') :
+def load_score_dict(text_file = 'sentiment.txt'):
     global txt_dictionary
-    with open(text_file) as open_text :
-        for line in open_text :
+    with open(text_file) as open_text:
+        for line in open_text:
             if line[0] != '#' and line[0] != '\n':
                 key, value = line.split()
                 txt_dictionary[key] = float(value)
@@ -24,28 +24,29 @@ Test section for load_score_dict() func
     load_score_dict('test_positive.txt')
 """
 
-def get_words(string_input) :
+
+def get_words(string_input):
     global output_string
     remove_punctuation_string = re.sub(r'[^\w\s]', '', string_input)
-        # how to use re in python ^^^
-        # https://datagy.io/python-remove-punctuation-from-string/
+# how to use re in python ^^^
+# https://datagy.io/python-remove-punctuation-from-string/
     lower_case_string = remove_punctuation_string.lower()
     new_string = lower_case_string.split()
     output_string = sorted(set(new_string), key=lambda x: new_string.index(x))
-
     return output_string
 
 
-def score_sentence(string_input, dictionary_input) :
+def score_sentence(string_input, dictionary_input):
     global output_string
     global txt_dictionary
     get_words(string_input)
     total = 0
-    for word in output_string :
-        if word not in dictionary_input :
+    for word in output_string:
+        if word not in dictionary_input:
             output_string.remove(word)
-            total += float(dictionary_input[word])
+            total += dictionary_input[word]
     return total
+
 
 """
 #my_sentence = "Grocery list:    3 boxes Land-o-lakes butter, Aunt Jemima's butter pancake mix"
@@ -58,7 +59,7 @@ score_sentence('welcome, welcome house!', scores)
 #inal_score = score_sentence(my_sentence, scores)
 """
 
-def open_string_file(txt_file) :
+def open_string_file(txt_file):
     file = open(txt_file)
     import_string = file.read().replace('\n',' ')
     file.close()
