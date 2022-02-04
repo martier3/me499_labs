53c8afdd6c0a66a8b67d326a5fb6f95062b9f819
martier3 <ericmartin657@gmail.com>
2022-02-03 21:28:30 -0800

    reformat PEP8

--- lab3/sentiment.py
+++ lab3/sentiment.py
@@ -2,28 +2,19 @@
 
 import re
 
-
-txt_dictionary = {}
+dictionary = {}
 output_string = []
 
 
 def load_score_dict(text_file = 'sentiment.txt'):
-    global txt_dictionary
+    global dictionary
     with open(text_file) as open_text:
         for line in open_text:
             if line[0] != '#' and line[0] != '\n':
                 key, value = line.split()
-                txt_dictionary[key] = float(value)
-        return txt_dictionary
+                dictionary[key] = float(value)
+        return dictionary
 
-
-"""
-Test section for load_score_dict() func
-    load_score_dict()
-    load_score_dict('test_neutral.txt')
-    load_score_dict('test_negative.txt')
-    load_score_dict('test_positive.txt')
-"""
 
 def get_words(string_input):
     global output_string
@@ -36,27 +27,18 @@ def get_words(string_input):
     return output_string
 
 
-def score_sentence(string_input, dictionary_input = 'sentiment.txt'):
+def score_sentence(string_input, dictionary_input):
     global output_string
-    global txt_dictionary
+    global dictionary
     get_words(string_input)
     load_score_dict(dictionary_input)
     total = 0
     for word in output_string:
-        if word not in txt_dictionary:
+        if word not in dictionary:
             output_string.remove(word)
-            total += txt_dictionary[word]
+            total += dictionary[word]
     return total, print(total)
 
-
-"""
-#my_sentence = "Grocery list:    3 boxes Land-o-lakes butter, Aunt Jemima's butter pancake mix"
-my_sentence = 'welcome, welcome to my house!'
-get_words(my_sentence)
-scores = {'welcome': 0.5,  'house': -0.25}
-score_sentence('welcome, welcome house!', scores)
-#inal_score = score_sentence(my_sentence, scores)
-"""
 
 def open_string_file(txt_file):
     file = open(txt_file)
