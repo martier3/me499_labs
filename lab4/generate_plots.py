f4c49b3ec96491da0f82b2ee922b593f62ee63ff
martier3 <ericmartin657@gmail.com>
2022-02-09 11:14:15 -0800

    working on the historgram for problem 2

--- lab4/generate_plots.py
+++ lab4/generate_plots.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-# Problem 1: Plot sping damper system
-import matplotlib.pyplot as plt
 from utils import *
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Problem 1: Plot sping damper system
 plt.plot(t, state[:,0])
 plt.xlabel('Time')
 plt.ylabel('Displacement')
 plt.title('Time vs Displacement')
 plt.savefig('Problem1.png')
+
+# Problem 2: Histogram of the gachapon problem
+n_bins = 15
+plt.hist(simulate_gachapon(15), density=1, bins=n_bins)
+plt.show()
--- lab4/utils.py
+++ lab4/utils.py
@@ -13,3 +13,17 @@ dt = 0.1
 
 smd = MassSpringDamper(m, k, c)
 state, t = smd.simulate(x, x_dot, t, dt)
+
+# Problem 2: Histogram of the gachapon problem
+import random
+
+def simulate_gachapon(n):
+    prize_pool = []
+    toy_list = [0, n-1]
+    if toy_list not in prize_pool:
+        prize_pool.append(random.randrange(0, n-1))
+    else:
+        return prize_pool, print(len(prize_pool)), print(len(toy_list))
+
+
+simulate_gachapon(15)
