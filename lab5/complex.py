25394c6ecef7f8356164946867698fde8ea2732b
martier3 <ericmartin657@gmail.com>
2022-02-17 17:44:19 -0800

    everthing works in complex.py except the division piece. I'm not sure how to make that one work yet. It outputs a number but it's wrong

--- lab5/complex.py
+++ lab5/complex.py
@@ -6,13 +6,64 @@ class Complex:
         self.re = float(re)
         self.im = float(im)
 
+
+    def __add__(self, other):
+        if isinstance(other, (float,int)):
+            return Complex(self.re + other, self.im)
+        else:
+            return Complex(self.re + other.re, self.im + other.im)
+
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+
+    def __sub__(self, other):
+        if isinstance(other, (float,int)):
+            return Complex(self.re - other, self.im)
+        else:
+            return Complex(self.re - other.re, self.im - other.im)
+
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+
+    def __mul__(self, other):
+        if isinstance(other, (float,int)):
+            return Complex(self.re * other, self.im * other)
+        else:
+            return Complex(self.re * other.re - self.im * other.im,
+                       self.im * other.re + self.re * other.im)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+
+        if isinstance(other, (float, int)):
+            r = float(other ** 2)
+            return Complex((self.re * other +self.im * 0) / r,
+                           (self.im * other -self.re * 0) / r)
+        else:
+            r = float(other.re ** 2 + other.im ** 2)
+            return Complex((self.re * other.re +self.im * other.im) / r,
+                           (self.im * other.re -self.re * other.im) / r)
+
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+
+
     def __str__(self):
         if self.im < 0:
-            return '(%s - %si)' % (self.re, self.im)
+            total = '(%s - %si)' % (self.re, abs(self.im))
+            return total
         else:
-            return '(%s + %si)' % (self.re, self.im)
+            total = '(%s + %si)' % (self.re, self.im)
+            return total
 
 
-print(Complex(-3, 2))
-print(Complex())
-print(Complex(3.4, -2.1))
+a = Complex(1.0, -3.0)
+print(a / Complex(4.0, 5.5))
+print(a / 3.5)
+print(a/ -2)
--- lab5/gachapon.py
+++ lab5/gachapon.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import statistics
-import argparse
 
 class GachaponSimulator:
 
@@ -57,12 +56,4 @@ class GachaponSimulator:
         return self.summary
 
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+# can't figure out command interface
