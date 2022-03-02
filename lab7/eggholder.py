01e883dcfb278eff25d0060f4da21852e23d2931
martier3 <ericmartin657@gmail.com>
2022-03-02 09:30:00 -0800

    getting started on lab7

---
+++ lab7/eggholder.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy import optimize
+
+
+def eggholder(x, y):
+    """
+    Takes in either a scalar x and y, or a 1-D numpy array for both x and y.
+    """
+    if (isinstance(x, np.ndarray) and not isinstance(y, np.ndarray)) or (
+            isinstance(y, np.ndarray) and not isinstance(x, np.ndarray)):
+        pass
+
+    result = -(y + 47) * np.sin(np.sqrt(np.abs(x / 2 + (y + 47)))) - x * np.sin(np.sqrt(np.abs(x - (y + 47))))
+    if isinstance(x, np.ndarray):
+        outside = (np.abs(x) > 512) | (np.abs(y) > 512)
+        result[outside] = 2000
+        return result
+    else:
+        return 2000 if (abs(x) > 512 or abs(y) > 512) else result
+
+def minimize_eggholder(guess, max_calls=100):
+    eggholder_minimized = optimize.fmin(func=eggholder, x0=guess, maxfun=max_calls)
+    return eggholder_minimized
+
+
---
+++ lab7/inertia.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+def compute_inertia_matrix(array, mass=1):
